878a5cada692cf1e0b80e3fbf8f5d9eff3d0f616
regislaine <regislaine.regis@ab-inbev.com>
2024-06-18 11:14:00 -0300

    feat: iniciando projeto

---
+++ desafio/src/Telefone/Iphone.java
@@ -0,0 +1,9 @@
+package Telefone;
+
+import Interface.Aparelhotelefonico;
+import Interface.NavegadorInternet;
+import Interface.ReprodutorMusical;
+
+public class Iphone implements ReprodutorMusical, Aparelhotelefonico, NavegadorInternet {
+    
+}
